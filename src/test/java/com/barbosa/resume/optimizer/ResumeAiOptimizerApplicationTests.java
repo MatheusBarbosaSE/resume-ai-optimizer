81f45c80e1091506e6974fffa0f5cb71e5b49969
Matheus Barbosa <matheusalolbarbosa@gmail.com>
2026-01-30 17:27:38 -0300

    chore: initial project setup and gitignore

---
+++ src/test/java/com/barbosa/resume/optimizer/ResumeAiOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbosa.resume.optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAiOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
